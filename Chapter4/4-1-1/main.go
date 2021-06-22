4899981f99719040b04aea61404fb7b491734679
JacobCN <coder211@163.com>
2021-06-22 13:17:58 +0800

    add or update code for chapter 4.1

--- Chapter4/4-1-1/main.go
+++ Chapter4/4-1-1/main.go
@@ -15,21 +15,49 @@ func main() {
 	foodsSlice5 := foodsSlice2[1:4]
 	fmt.Println(len(foodsSlice5))
 	fmt.Println(cap(foodsSlice5))
+
+	println("\n演示切片扩容")
 	foodSlice4 := []string{"红烧肉"}
 	fmt.Println(foodSlice4)
-	fmt.Println(fmt.Sprintf("Len:%d", len(foodSlice4)))
+	fmt.Print(fmt.Sprintf("Len:%d\t", len(foodSlice4)))
 	fmt.Println(fmt.Sprintf("Cap:%d", cap(foodSlice4)))
-
 	foodSlice4 = append(foodSlice4, "清蒸鱼")
-	fmt.Println(fmt.Sprintf("Len:%d", len(foodSlice4)))
+	fmt.Println(foodSlice4)
+	fmt.Print(fmt.Sprintf("Len:%d\t", len(foodSlice4)))
 	fmt.Println(fmt.Sprintf("Cap:%d", cap(foodSlice4)))
 	foodSlice4 = append(foodSlice4, "溜大虾")
-	fmt.Println(fmt.Sprintf("Len:%d", len(foodSlice4)))
+	fmt.Println(foodSlice4)
+	fmt.Print(fmt.Sprintf("Len:%d\t", len(foodSlice4)))
 	fmt.Println(fmt.Sprintf("Cap:%d", cap(foodSlice4)))
 	foodSlice4 = append(foodSlice4, "蒸螃蟹")
-	fmt.Println(fmt.Sprintf("Len:%d", len(foodSlice4)))
+	fmt.Println(foodSlice4)
+	fmt.Print(fmt.Sprintf("Len:%d\t", len(foodSlice4)))
 	fmt.Println(fmt.Sprintf("Cap:%d", cap(foodSlice4)))
 	foodSlice4 = append(foodSlice4, "鲍鱼粥")
-	fmt.Println(fmt.Sprintf("Len:%d", len(foodSlice4)))
+	fmt.Println(foodSlice4)
+	fmt.Print(fmt.Sprintf("Len:%d\t", len(foodSlice4)))
 	fmt.Println(fmt.Sprintf("Cap:%d", cap(foodSlice4)))
+
+	// 新添加的实验代码
+	var a1 = [8]string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	var a2 = [...]string{"A","B","C"}
+	fmt.Printf("a1 =\t%s\n", a1)
+	fmt.Printf("a2 =\t%s\n", a2)
+	// 没有python那么强大的切片功能
+	// 1.基于数组创建切片
+	fmt.Printf("a1[2:6] =\t%s\n", a1[2:6])
+	fmt.Printf("a1[:6] =\t%s\n", a1[:6])
+	fmt.Printf("a1[2:] =\t%s\n", a1[2:])
+	fmt.Printf("a1[:] =\t\t%s\n", a1[:])
+	// 2.直接创建切片
+	var s1 = []string{"A","B","C"}
+	fmt.Printf("s1=%s\n", s1)
+	// 3.函数make([] type, len, <cap>)创建切片
+	var s2 = make([]string, 3, 5)
+	fmt.Printf("s2=%s\tlen(s2)=%d\tcap(s2)=%d\n", s2, len(s2), cap(s2))
+	for i, e := range s2{
+		fmt.Printf("%d -> [%s]\n", i, e)
+	}
+	s2[2], s2[1] = "-2", "-1"
+	fmt.Printf("s2=%05s\tlen(s2)=%d\tcap(s2)=%d\n", s2, len(s2), cap(s2))
 }
--- Chapter4/4-1-4/main.go
+++ Chapter4/4-1-4/main.go
@@ -18,4 +18,19 @@ func main() {
 	copy(foodsSlice3, foodsSlice7)
 	fmt.Println(foodsSlice3)
 	fmt.Println(foodsSlice7)
+
+	// 新添加的实验代码
+	var a1 = [8]string{"a", "b", "c", "d", "e", "f", "g", "h"}
+	var a2 = [...]string{"A","B","C"}
+	fmt.Printf("a1 =\t%s\n", a1)
+	fmt.Printf("a2 =\t%s\n", a2)
+	// 1 从切片种提取切片
+	var a11 = a1[1:]
+	fmt.Printf("%s\n", a11)
+	// 2 copy(dest, src []Type) int
+	a21 := a2[1:]
+	a12 := a1[2:]
+	copy(a21, a12)
+	fmt.Printf("%s <- %s\n", a21, a12)
+
 }
