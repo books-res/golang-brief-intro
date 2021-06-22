402c87c71d52785985cbfc670234d26c760c5731
JacobCN <coder211@163.com>
2021-06-22 15:30:12 +0800

    add or update code for chapter 4

--- Chapter4/4-3/main.go
+++ Chapter4/4-3/main.go
@@ -4,6 +4,7 @@ import "fmt"
 
 func main() {
 	foods := []string{"红烧肉", "清蒸鱼", "熘大虾", "蒸螃蟹", "蒜蓉粉丝扇贝"}
+	//panic: runtime error: index out of range [5] with length 5
 	//fmt.Println(foods[5])
 	fmt.Println(foods[len(foods)-1])
 }
--- Chapter4/4-4/main.go
+++ Chapter4/4-4/main.go
@@ -3,17 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	//foods := []string{"红烧肉", "清蒸鱼", "溜大虾", "蒸螃蟹", "鲍鱼粥"}
-	//foods2 := make([]*string, len(foods))
+	foods := []string{"红烧肉", "清蒸鱼", "溜大虾", "蒸螃蟹", "鲍鱼粥"}
+	foods2 := make([]*string, len(foods))
+	// 错误的示例
 	//for i, value := range foods {
 	//	foods2[i] = &value
 	//}
-	//fmt.Println(foods[0], foods[1], foods[2], foods[3], foods[4])
-	//fmt.Println(*foods2[0], *foods2[1], *foods2[2], *foods2[3], *foods2[4])
-
-	//解决上面代码的错误，可以参照书中的例子
-	foods := []string{"红烧肉", "清蒸鱼", "溜大虾", "蒸螃蟹", "鲍鱼粥"}
-	foods2 := make([]*string, len(foods))
+	// 正确的示例
 	for i, _ := range foods {
 		foods2[i] = &foods[i]
 	}
